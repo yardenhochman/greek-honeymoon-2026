865eee3cda750e5bdb5d5ffb0463dc903b354b11
Yarden Hochman <yyyarden@gmail.com>
2026-09-15 23:38:31 +0300

    Group ferry documents in one card with descriptive links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "greek-honeymoon-public-2026-09-15-v4";
+const CACHE = "greek-honeymoon-public-2026-09-15-v5";
 const FILES = ["./assets/chania.jpg", "./assets/perissa.jpg", "./assets/greek-table.jpg", "./practical.html", "./experiences.html", "./phone.js", "./", "./index.html", "./app.js", "./styles.css", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./icon-512.png", "./assets/map-day-01.png", "./assets/map-day-02.png", "./assets/map-day-03.png", "./assets/map-day-04.png", "./assets/map-day-05.png", "./assets/map-day-06.png", "./assets/map-day-07.png", "./assets/map-day-08.png", "./assets/map-day-10.png", "./assets/map-day-11.png"];
 const urls = new Set(FILES.map(path => new URL(path, self.registration.scope).href));
 self.addEventListener("install", event => {
